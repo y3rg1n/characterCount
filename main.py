f18d37e13580bf4338347fc3ddfb2c3d5317c8a4
y3rg1n <mmbartczak@gmail.com>
2024-09-23 11:22:24 +0200

    created character count main file

---
+++ main.py
@@ -0,0 +1,10 @@
+message = "A practical programming course for office workers, academics, and administrators who want to improve their productivity."
+count = {}
+
+for character in message.upper():
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+
+print(sorted(dict.items(count)))
+
